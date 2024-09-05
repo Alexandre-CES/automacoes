368208db10bb20e0ec53465549a82f0e1da2b2d7
Alexandre-CES <147560376+Alexandre-CES@users.noreply.github.com>
2024-09-05 20:01:28 -0300

    navigation

--- manipulacao_imagem/imagem.py
+++ manipulacao_imagem/imagem.py
@@ -2,51 +2,86 @@ from PIL import Image
 import math
 
 def Main():
+
+    image_file = input('Nome do arquivo da imagem: ')
+
+    print('Qual filtro deseja?: ')
+    print('[0]Cancelar - [1]Sobel')
+    filter_opt = choose_opt(0,1)
+    if filter_opt == 0:
+        return
+
     # Carrega a imagem
-    with Image.open('imagens/arvore.jpg') as img:
+    with Image.open(f'imagens/{image_file}') as img:
 
-        img = img.convert('L')
+        img_result = None
 
-        # Obtém os dados dos pixels da original
-        pixels = img.load()
+        if filter_opt == 1:
+            
+            img_result = sobel(img)
 
-        # Obtém as dimensões da imagem
-        largura, altura = img.size
+        img_result.save('resultado.jpg')
 
-        Gx = [
+def choose_opt(min,max):
+    opt = 0
+    while True:
+        opt = int(input())
+        try:
+            if opt >= min and opt <= max:
+                break
+            else:
+                print('Opção inválida, tente novamente: ')
+
+        except ValueError:
+            print('Opção inválida, tente novamente: ')
+
+    return opt
+
+def sobel(img):
+
+    #grayscale
+    img = img.convert('L')
+
+    # Obtém os dados dos pixels da original
+    pixels = img.load()
+
+    # Obtém as dimensões da imagem
+    largura, altura = img.size
+
+    #Cópia da original
+    img_result = Image.new('L', (largura, altura))
+    pixels_result = img_result.load()
+    
+    Gx = [
             [1,0,-1],
             [2,0,-2],
             [1,0,-1]
         ]
 
-        Gy = [
-            [1,2,1],
-            [0,0,0],
-            [-1,-2,-1]
-        ]
+    Gy = [
+        [1,2,1],
+        [0,0,0],
+        [-1,-2,-1]
+    ]
 
-        #Para não alterar a original
-        img_resultado = Image.new('L', (largura, altura))
-        pixels_resultado = img_resultado.load()
+    #iterar sob todos os pixels da imagem
+    for y in range(1,altura - 1):
+        for x in range(1,largura - 1):
+            GxSum = 0
+            GySum = 0
 
-        #iterar sob todos os pixels da imagem
-        for y in range(1,altura - 1):
-            for x in range(1,largura - 1):
-                GxSum = 0
-                GySum = 0
+            #captar pixels adjacentes
+            for i in range(3):
+                for j in range(3):
+                    GxSum += Gx[i][j] * pixels[x + j - 1, y + i - 1]
+                    GySum += Gy[i][j] * pixels[x + j - 1, y + i - 1]
 
-                #captar pixels adjacentes
-                for i in range(3):
-                    for j in range(3):
-                        GxSum += Gx[i][j] * pixels[x + j - 1, y + i - 1]
-                        GySum += Gy[i][j] * pixels[x + j - 1, y + i - 1]
+            G = math.sqrt(GxSum**2 + GySum**2)
 
-                G = math.sqrt(GxSum**2 + GySum**2)
+            G = int(G / math.sqrt(2 * (255**2)) * 255)
 
-                G = int(G / math.sqrt(2 * (255**2)) * 255)
-
-                pixels_resultado[x,y] = G
-
-        img_resultado.save('arvore_sobel.jpg')
+            pixels_result[x,y] = G
+        
+    return img_result
 
 Main()
